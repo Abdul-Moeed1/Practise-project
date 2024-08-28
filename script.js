645ddef24a66b5f4d1ff2c2cad4757453704aa9a
Abdul-Moeed1 <91484014+Abdul-Moeed1@users.noreply.github.com>
2024-08-28 17:32:55 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const burger = document.getElementById('burger');
+const navLinks = document.getElementById('nav-links');
+
+burger.addEventListener('click', () => {
+    navLinks.classList.toggle('show');
+});
+
+
+
